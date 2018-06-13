3e81609d102d3014feea0a60166778e5a716e2bb
JohRus <johan.rusvik@bekk.no>
2018-06-13 13:53:02 +0200

    KAIZEN-0 Fjerner ubrukt kode

--- src/innhold.tsx
+++ src/innhold.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router';
 import Startside from './startside/startside';
-import { AppState } from './redux/reducer';
 
-interface StateProps {
-}
-
-type InnholdProps = StateProps & RouteComponentProps<any>; // tslint:disable-line:no-any
+type InnholdProps = RouteComponentProps<any>; // tslint:disable-line:no-any
 
 class Innhold extends React.Component<InnholdProps> {
     constructor(props: InnholdProps) {
@@ -30,8 +25,4 @@ class Innhold extends React.Component<InnholdProps> {
     }
 }
 
-const mapStateToProps = (state: AppState): StateProps => ({
-    dummy: '',
-});
-
-export default withRouter(connect(mapStateToProps)(Innhold));
+export default withRouter(Innhold);
