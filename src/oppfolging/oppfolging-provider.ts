d735d0afe2388bce0dc01b258e8eca4f9b88c83c
Johan Rusvik <johan.rusvik@nav.no>
2018-06-18 13:39:58 +0200

    KAIZEN-0 Flytter datalast av oppfolging inn i komponenten det brukes

--- src/oppfolging/oppfolging-provider.ts
+++ src/oppfolging/oppfolging-provider.ts
@@ -1,23 +1,17 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from '../redux/dispatch-type';
-import { AppState } from '../redux/reducer';
 import { hentOppfolging } from './oppfolging-duck';
-import { Status } from '../api/datalaster';
 
 interface OwnProps {
     children: React.ReactElement<any>; // tslint:disable-line:no-any
-}
-
-interface StateProps {
-    oppfolgingStatus: Status;
 }
 
 interface DispatchProps {
     doHentOppfolging: () => void;
 }
 
-type OppfolgingProviderProps = OwnProps & StateProps & DispatchProps;
+type OppfolgingProviderProps = OwnProps & DispatchProps;
 
 class OppfolgingProvider extends React.Component<OppfolgingProviderProps> {
     constructor(props: OppfolgingProviderProps) {
@@ -29,19 +23,12 @@ class OppfolgingProvider extends React.Component<OppfolgingProviderProps> {
     }
 
     render() {
-        if (this.props.oppfolgingStatus !== Status.OK) {
-            return null;
-        }
         return this.props.children;
     }
 }
-
-const mapStateToProps = (state: AppState): StateProps => ({
-    oppfolgingStatus: state.oppfolging.status,
-});
 
 const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
     doHentOppfolging: () => hentOppfolging()(dispatch)
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(OppfolgingProvider);
+export default connect(null, mapDispatchToProps)(OppfolgingProvider);
--- src/startside/kontakte-nav.tsx
+++ src/startside/kontakte-nav.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import { AppState } from '../redux/reducer';
 import { connect } from 'react-redux';
-import { Undertittel, Normaltekst } from 'nav-frontend-typografi';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import 'nav-frontend-lenker-style';
 import 'nav-frontend-knapper-style';
 import './kontakte-nav.less';
+import { OppfolgingState } from '../oppfolging/oppfolging-duck';
+import Datalaster from '../api/datalaster';
 
 interface StateProps {
-    underOppfolging: boolean;
+    oppfolging: OppfolgingState;
 }
 
 export type KontakteNavProps = StateProps;
@@ -21,43 +23,46 @@ class KontakteNAV extends React.Component<KontakteNavProps> {
     render() {
         const lenkeAktivitetsplan = '/aktivitetsplan';
         const lenkeInfo = 'https://www.nav.no/no/Person/Arbeid/Oppfolging+og+tiltak+for+a+komme+i+jobb/Nyheter/lag-din-egen-aktivitetsplan-p%C3%A5-ditt-nav'; // tslint:disable-line
-        const {underOppfolging} = this.props;
+        const {oppfolging} = this.props;
         return (
-            <section className="kontakte-nav">
+            <Datalaster avhengigheter={[oppfolging]}>
+                <section className="kontakte-nav">
 
-                <Undertittel className="blokk-xs">Snakk med oss om tiltak</Undertittel>
+                    <Undertittel className="blokk-xs">Snakk med oss om tiltak</Undertittel>
 
-                { underOppfolging && (
-                    <>
-                        <Normaltekst>
-                            Ta gjerne kontakt med NAV-kontoret hvis du vil drøfte dine muligheter for tiltak.
-                            På nav.no kan du lage din egen aktivitetsplan og du kan starte en dialog med veilederen
-                            din når du er inne i planen.
-                        </Normaltekst>
-                        <a className="knapp knapp--hoved" href={lenkeAktivitetsplan}>
-                            Gå til aktivitetsplanen
-                        </a>
-                    </>
-                )}
-                { !underOppfolging && (
-                    <>
-                        <Normaltekst>
-                            Ta gjerne kontakt med NAV-kontoret hvis du vil drøfte dine muligheter for tiltak.
-                            På nav.no kan du lage din egen aktivitetsplan og du kan starte en dialog med veilederen din
-                            når du er inne i planen.
-                        </Normaltekst>
-                        <a className="knapp knapp--hoved" href={lenkeInfo}>
-                            Les mer om aktivitetsplanen
-                        </a>
-                    </>
-                )}
-            </section>
+                    {oppfolging.underOppfolging && (
+                        <>
+                            <Normaltekst>
+                                Ta gjerne kontakt med NAV-kontoret hvis du vil drøfte dine muligheter for tiltak.
+                                På nav.no kan du lage din egen aktivitetsplan og du kan starte en dialog med veilederen
+                                din når du er inne i planen.
+                            </Normaltekst>
+                            <a className="knapp knapp--hoved" href={lenkeAktivitetsplan}>
+                                Gå til aktivitetsplanen
+                            </a>
+                        </>
+                    )}
+                    {!oppfolging.underOppfolging && (
+                        <>
+                            <Normaltekst>
+                                Ta gjerne kontakt med NAV-kontoret hvis du vil drøfte dine muligheter for tiltak.
+                                På nav.no kan du lage din egen aktivitetsplan og du kan starte en dialog med veilederen
+                                din
+                                når du er inne i planen.
+                            </Normaltekst>
+                            <a className="knapp knapp--hoved" href={lenkeInfo}>
+                                Les mer om aktivitetsplanen
+                            </a>
+                        </>
+                    )}
+                </section>
+            </Datalaster>
         );
     }
 }
 
 const mapStateToProps = (state: AppState): StateProps => ({
-    underOppfolging: state.oppfolging.underOppfolging
+    oppfolging: state.oppfolging,
 });
 
 export default connect(mapStateToProps)(KontakteNAV);
