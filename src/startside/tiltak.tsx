712793338e88eab4a392376adde0ee4d2efc3383
JohRus <johan.rusvik@bekk.no>
2018-06-26 10:07:56 +0200

    KAIZEN-0 Flytter tiltakconfig til egen fil

--- src/startside/tiltak.tsx
+++ src/startside/tiltak.tsx
@@ -3,25 +3,9 @@ import { Element, EtikettLiten, Innholdstittel, Normaltekst } from 'nav-frontend
 import './tiltak.less';
 import Lenkepanel from 'nav-frontend-lenkepanel/lib/index';
 import tekst from '../finn-tekst';
+import tiltakListe from '../ledetekster/tiltak-config';
 
 export function Tiltak() {
-
-    const tiltakListe = [];
-    tiltakListe.push({
-        tittel: 'Lønnstilskudd',
-        hva: 'Har du en arbeidsgiver som er interessert i å ansette deg? Trenger du tid for å kunne yte full innsats? NAV kan dekke deler av lønnen din en periode. Hvor mye NAV skal dekke, vurderer du sammen med arbeidsgiveren og NAV.', // tslint:disable-line:max-line-length
-        ikon: require('../ikoner/lonnstilskudd.svg'),
-        style: 'style',
-        url: 'https://www.nav.no/no/Person/Arbeid/Oppfolging+og+tiltak+for+a+komme+i+jobb/Tiltak+for+a+komme+i+jobb/Tilskudd+til+lonnsutgifter', // tslint:disable-line:max-line-length
-    });
-    tiltakListe.push({
-        tittel: 'Oppfølging',
-        hva: 'Trenger du mer støtte enn det NAV kan gi for å komme i jobb? Da kan dette være noe for deg. Du kan få råd og veiledning for å bli en bedre jobbsøker, eller hjelp til å finne og kontakte arbeidsgivere. Eller kanskje du trenger støtte til noe annet for å komme i jobb?', // tslint:disable-line:max-line-length
-        ikon: require('../ikoner/oppfolging.svg'),
-        style: 'style',
-        url: 'https://www.nav.no/no/Person/Arbeid/Oppfolging+og+tiltak+for+a+komme+i+jobb/Tiltak+for+a+komme+i+jobb/Relatert+innhold/oppf%C3%B8lging', // tslint:disable-line:max-line-length
-    });
-
     return (
         <section className="tiltak-oversikt blokk-m">
             {tiltakListe.map((tiltak) =>
