a7552f53b1252ecca24e31ab2550a257825169c5
JohRus <johan.rusvik@bekk.no>
2018-07-12 12:20:24 +0200

    FO-1204 Legger til datalaster for arbeidsforhold rundt tittel

--- src/startside/tittel.tsx
+++ src/startside/tittel.tsx
@@ -3,22 +3,28 @@ import { Sidetittel } from 'nav-frontend-typografi';
 import Tekst from '../finn-tekst';
 import { connect } from 'react-redux';
 import { AppState } from '../redux/reducer';
+import Datalaster from '../api/datalaster';
+import { ArbeidsforholdState } from '../arbeidsforhold/arbeidsforhold-duck';
 
 interface StateProps {
     harArbeidsgiver: boolean;
+    arbeidsforhold: ArbeidsforholdState;
 }
 
-export function Tittel({harArbeidsgiver}: StateProps) {
+export function Tittel({harArbeidsgiver, arbeidsforhold}: StateProps) {
     const tekstId = harArbeidsgiver ? 'tittel-hararbeidsgiver' : 'tittel-utenarbeidsgiver';
     return (
-        <section className="blokk-xs">
-            <Sidetittel><Tekst id={tekstId}/></Sidetittel>
-        </section>
+        <Datalaster avhengigheter={[arbeidsforhold]}>
+            <section className="blokk-xs">
+                <Sidetittel><Tekst id={tekstId}/></Sidetittel>
+            </section>
+        </Datalaster>
     );
 }
 
 const mapStateToProps = (state: AppState): StateProps => ({
     harArbeidsgiver: state.arbeidsforhold.data.harArbeidsgiver,
+    arbeidsforhold: state.arbeidsforhold,
 });
 
 export default connect(mapStateToProps)(Tittel);
