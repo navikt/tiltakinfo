37a020d428fa22140bbc972c2873bdd752f56153
JohRus <johan.rusvik@bekk.no>
2018-07-13 10:07:41 +0200

    FO-1204 Refaktorerer strukturen til maal

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -5,12 +5,13 @@ import statusReducer, { StatusState } from '../status/status-duck';
 import arbeidsforholdReducer, { ArbeidsforholdState } from '../arbeidsforhold/arbeidsforhold-duck';
 import genericDuck from './generic-duck';
 import { ActionType } from './actions';
+import { MAAL_OPTIONS } from '../startside/ingress';
 
 interface MaalState {
-    maal: string;
+    id: string;
 }
 
-export const maalDuck = genericDuck<MaalState, ActionType.SETT_MAAL>({maal: ''}, ActionType.SETT_MAAL);
+export const maalDuck = genericDuck<MaalState, ActionType.SETT_MAAL>({id: MAAL_OPTIONS[0]}, ActionType.SETT_MAAL);
 
 export interface AppState {
     unleash: UnleashState;
