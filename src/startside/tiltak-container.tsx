5ecec1304b5cc9c117c5b40c6a14a1ecbd08004f
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2018-09-20 14:56:03 +0200

    FO-1534 legg til veilederpanel for arbeidsledige med arbeidsgiver

--- src/startside/tiltak-container.tsx
+++ src/startside/tiltak-container.tsx
@@ -7,7 +7,10 @@ import { MaalOption, maalTiltakMap } from './maal-tiltak-map';
 import { AppState } from '../redux/reducer';
 import { connect } from 'react-redux';
 import { SykmeldingerState } from '../sykmeldinger/sykmeldinger-duck';
+import Veilederpanel from 'nav-frontend-veilederpanel';
 import TiltakKomponent from './tiltak-komponent';
+
+const veilederBilde = require('../ikoner/veileder-dame.svg');
 
 interface StateProps {
     sykmeldinger: SykmeldingerState;
@@ -21,17 +24,28 @@ class TiltakContainer extends React.Component<StateProps> {
         const tiltakSomVises: Tiltak[] = sykmeldinger.data.harArbeidsgiver ?
             maalTiltakMap[maalId].map((tiltakId: TiltakId) => tiltakConfig(tiltakId)) :
             [tiltakConfig(TiltakId.LONNSTILSKUDD), tiltakConfig(TiltakId.OPPFOLGING)];
+
         return (
-            <section className="tiltak-oversikt">
-                <Undertittel className="tiltak-overskrift blokk-s">
-                    <Tekst id={'informasjon-totiltak'}/>
-                </Undertittel>
-                <div className="tiltak-liste">
-                    {tiltakSomVises.map((tiltak: Tiltak) =>
-                        <TiltakKomponent key={tiltak.tittel} tiltak={tiltak} />
-                    )}
-                </div>
-            </section>
+            <>
+                { (sykmeldinger.data.harArbeidsgiver) && (maalId === MaalOption.SAMME_ARBEIDSGIVER) ?
+                    <section className="tiltak-ingress">
+                        <Veilederpanel svg={<img src={veilederBilde}/>} type="normal" kompakt={true}>
+                                <Tekst id="veileder-maal-samme-arbeidsgiver"/>
+                        </Veilederpanel>
+                    </section>
+                    :
+                    null }
+                <section className="tiltak-oversikt">
+                    <Undertittel className="tiltak-overskrift blokk-s">
+                        <Tekst id={'informasjon-totiltak'}/>
+                    </Undertittel>
+                    <div className="tiltak-liste">
+                        {tiltakSomVises.map((tiltak: Tiltak) =>
+                            <TiltakKomponent key={tiltak.tittel} tiltak={tiltak} />
+                        )}
+                    </div>
+                </section>
+            </>
         );
     }
 }
